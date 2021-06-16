c50197538796cbe7cb6f333851c28ad2efc5fce7
nvardp <72966302+nvardp@users.noreply.github.com>
2021-06-16 14:48:25 +0400

    Add files via upload

--- My_SQL/MySQL.h
+++ My_SQL/MySQL.h
@@ -8,17 +8,17 @@
 #include <Windows.h>
 
 namespace fs = std::filesystem;
-using namespace std;
 
-const string _EXIT = "exit";
-const string _CREATE_CMD = "create"; //+
-const string _INSERT_CMD = "insert"; //+
-const string _UPDATE_CMD = "update";
-const string _DROP_CMD = "drop"; //+
-const string _DELETE_CMD = "delete"; //+
-const string _SELECT_CMD = "select"; //+
-const string _WHERE_CMD = "where"; //+
-const string _ALL = "*";
+
+const std::string _EXIT = "exit";
+const std::string _CREATE_CMD = "create"; 
+const std::string _INSERT_CMD = "insert"; 
+const std::string _UPDATE_CMD = "update";
+const std::string _DROP_CMD = "drop"; 
+const std::string _DELETE_CMD = "delete";
+const std::string _SELECT_CMD = "select"; 
+const std::string _WHERE_CMD = "where"; 
+const std::string _ALL = "*";
 
 enum QueryIndex
 {
@@ -31,27 +31,27 @@ class MySql {
 private:
 	using Entry = std::map<std::string, std::string>;  // one obj= one map//row
 	using Table = std::vector<Entry>; //one file
-	using Query = std::vector<string>;
+	using Query = std::vector<std::string>;
 
 public:
 	MySql();
-	static bool isQueryOK(const string& other);
-	void Execute(const string& other);
+	static bool isQueryOK(const std::string& other);
+	void Execute(const std::string& other);
 private:
-	void create_file(const string& name);
+	void create_file(const std::string& name);
 	void Insert(const Query& query);
 	bool drop_table(const Query& query);
 	int delete_all_found(const Query& query);
 	int update(const Query& query);
 	void show_table(const Query& other);
-	string get_col_name(string other);
-	string get_col_value(string other);
-	string get_path_of_exe();
-	Query parse(string other);
+	std::string get_col_name(std::string other);
+	std::string get_col_value(std::string other);
+	std::string get_path_of_exe();
+	Query parse(std::string other);
 	void write_in_file(const Query& vec);
 	void update_map(const Query& query);
-	bool exist(const std::map<string, Table>& table, const string& name) const;
-	void write_after_operation(const Table& other, const string& name);
+	bool exist(const std::map<std::string, Table>& table, const std::string& name) const;
+	void write_after_operation(const Table& other, const std::string& name);
 private:
 	std::map<std::string, Table> tables; //name of file, contains
 	std::string path;
